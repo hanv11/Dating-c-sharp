75ed69fdc580f6fa330bbf3eec6dd37cd80d9003
hanv11 <hanguyen11mm@gmail.com>
2020-01-17 22:52:18 +0700

    add enums class

---
+++ Dating/Structure/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dating.Constant
+{
+    public class Enums
+    {
+        public enum NamSinhAmLich
+        {
+            // vi du nhu vay
+            CanhDan = 0,
+
+        }
+    }
+}
